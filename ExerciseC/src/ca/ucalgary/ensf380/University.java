480851d6aa3e7fe0e57d1a0317eea8237723d122
Faris <faris.janjua@ucalgary.ca>
2024-07-18 16:21:46 -0600

    Final ExerciseC (fixed Scenario)

--- ExerciseC/src/ca/ucalgary/ensf380/University.java
+++ ExerciseC/src/ca/ucalgary/ensf380/University.java
@@ -1,58 +1,29 @@
 package ca.ucalgary.ensf380;
 
-import java.util.Scanner;
-
 public class University {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
       
         Address professor1Address = new Address("123 University St", "T2N 1N4", "Alberta", "Canada");
         Address professor2Address = new Address("456 University St", "T2N 1N8", "Alberta", "Canada");
         Address student1Address = new Address("789 College Ave", "T2N 2N4", "Alberta", "Canada");
         Address student2Address = new Address("810 Campus Dr", "T2N 2N8", "Alberta", "Canada");
 
-      
         Professor professor1 = new Professor("Travis", "555-5555", "travis@university.ca", professor1Address, 1, "P001", 200000.0);
         Professor professor2 = new Professor("Drake", "555-1234", "drake@university.ca", professor2Address, 2, "P002", 176000.0);
 
-  
-        Student student1 = new Student("Kanye", "555-9101", "kanye@university.ca", student1Address, 3, "S001", professor1, 85.0);
-        Student student2 = new Student("Kendrick", "555-1122", "kendrick@university.ca", student2Address, 4, "S002", professor2, 90.0);
+      
+        Student student1 = new Student("Kanye", "555-9101", "kanye@university.ca", student1Address, 3, "S001", professor1, 64.0);
+        Student student2 = new Student("Kendrick", "555-1122", "kendrick@university.ca", student2Address, 4, "S002", professor2, 93.0);
+
+        System.out.println("Student 1: " + student1.getName() + ", Address: " + student1.getAddress().getStreet() + ", Grade: " + student1.getAverageMark());
+        System.out.println("Student 2: " + student2.getName() + ", Address: " + student2.getAddress().getStreet() + ", Grade: " + student2.getAverageMark());
+       
+        System.out.println("Professor 1: " + professor1.getName() + ", Address: " + professor1.getAddress().getStreet() + ", Salary: " + professor1.getSalary());
+        System.out.println("Professor 2: " + professor2.getName() + ", Address: " + professor2.getAddress().getStreet() + ", Salary: " + professor2.getSalary());
+
         
-        while (true) {
-            System.out.println("Menu:");
-            System.out.println("1. Book meeting with Professor Travis");
-            System.out.println("2. Book meeting with Professor Drake");
-            System.out.println("3. Print student details");
-            System.out.println("4. Print professor details");
-            System.out.println("5. Exit");
-            System.out.print("Choose an option: ");
-            int choice = scanner.nextInt();
-            scanner.nextLine(); 
-            
-            switch (choice) {
-                case 1:
-                    System.out.println("Meeting booked with Professor Travis.");
-                    break;
-                case 2:
-                    System.out.println("Meeting booked with Professor Drake.");
-                    break;
-                case 3:
-                    System.out.println("Student 1: " + student1.getName() + ", Address: " + student1.getAddress().getStreet());
-                    System.out.println("Student 2: " + student2.getName() + ", Address: " + student2.getAddress().getStreet());
-                    break;
-                case 4:
-                    System.out.println("Professor 1: " + professor1.getName() + ", Address: " + professor1.getAddress().getStreet());
-                    System.out.println("Professor 2: " + professor2.getName() + ", Address: " + professor2.getAddress().getStreet());
-                    break;
-                case 5:
-                    System.out.println("Exiting..");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("Invalid option, try again.");
-            }
-        }
+        System.out.println("Booking a meeting with Professor Travis.");
+        System.out.println("Booking a meeting with Professor Drake.");
     }
 }
+
